537de6a6756b7a7007073d38e7624462a4c8c4d1
Matt Klich <matt.klich@readytalk.com>
2014-06-25 15:42:08 -0600

    Added installation script - used to be a gist

---
+++ dotfoo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -xe
+
+##
+# Homeshick
+##
+if [[ ! -d $HOME/.homesick/repos/homeshick ]]; then
+  git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+else
+  ( cd $HOME/.homesick/repos/homeshick; git pull )
+fi
+type homeshick &> /dev/null || source $HOME/.homesick/repos/homeshick/homeshick.sh
+
+repos="elementalvoid/dotfiles"
+for repo in ${repos}; do
+  if homeshick list | grep -q ${repo}; then
+    homeshick pull ${repo/*\//}
+  else
+    homeshick clone ${repo}
+  fi
+done
+
+##
+# Vim
+##
+if [[ -d ~/.vim ]]; then
+  ( cd ~/.vim; git pull )
+else
+    git clone git@github.com:elementalvoid/vimrc.git ~/.vim
+fi
+( cd ~/.vim; ./install.sh )
