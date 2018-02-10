4f0b0d62ba244ebe8672183e3c4116d647d55610
Matt Klich <mklich@shutterstock.com>
2018-02-10 15:04:13 -0700

    fzf and alacritty

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ else
 fi
 type homeshick &> /dev/null || source $HOME/.homesick/repos/homeshick/homeshick.sh
 
-repos="elementalvoid/dotfiles elementalvoid/liquidprompt"
+repos="elementalvoid/dotfiles elementalvoid/liquidprompt junegunn/fzf"
 for repo in ${repos}; do
   if homeshick list | grep -q ${repo}; then
     homeshick --batch pull ${repo/*\//}
@@ -20,6 +20,11 @@ for repo in ${repos}; do
   fi
 done
 homeshick --force link
+
+##
+# fzf
+##
+~/.homesick/repos/fzf/install --bin
 
 ##
 # Vim
@@ -31,19 +36,6 @@ else
     git clone https://github.com/elementalvoid/vimrc.git ~/.vim
 fi
 ( cd ~/.vim; ./install.sh )
-
-if [[ -n ${SSH_CLIENT} ]]; then
-  echo "Skipping powerline fonts installation on remote host (ssh)..."
-elif command -pv fc-cache > /dev/null 2>&1; then
-  if [[ -d ~/.fonts-powerline ]]; then
-    ( cd ~/.fonts-powerline; git pull; ./install.sh )
-  else
-    git clone https://github.com/Lokaltog/powerline-fonts.git ~/.fonts-powerline
-    ( cd ~/.fonts-powerline; ./install.sh )
-  fi
-else
-  echo "Skipping powerline fonts installation (missing fc-cache)..."
-fi
 
 ##
 # Install sslyze
