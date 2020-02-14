c178d70236de6d7df77dbbf13f8ee01bd55a7cdf
Matt Klich <matt@elementalvoid.com>
2020-02-14 13:54:43 -0700

    asdf java home support

--- home/.zshrc.d/asdf.plugin.zsh
+++ home/.zshrc.d/asdf.plugin.zsh
@@ -6,4 +6,8 @@ if [[ -d ${asdf_location} ]]; then
   source ${asdf_location}/completions/asdf.bash
 fi
 unset asdf_location
+
+if [[ -f ~/.asdf/plugins/java/set-java-home.sh ]]; then
+  . ~/.asdf/plugins/java/set-java-home.sh
+fi
 # vim: set ft=sh ts=2 sw=2 tw=0 :
