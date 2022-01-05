33ac75908938ff264912d67e53863f4be04282b5
Matt Klich <matt@elementalvoid.com>
2022-01-04 18:42:10 -0700

    update installer

--- install.sh
+++ install.sh
@@ -12,24 +12,10 @@ if [[ ! -f ~/.ssh/id_rsa ]]; then
 fi
 
 ##
-# Homeshick
+# Chezmoi
 ##
-if [[ ! -d ~/.homesick/repos/homeshick ]]; then
-  git clone git@github.com:andsens/homeshick.git ~/.homesick/repos/homeshick
-else
-  ( cd ~/.homesick/repos/homeshick; git pull )
-fi
-type homeshick &> /dev/null || source ~/.homesick/repos/homeshick/homeshick.sh
-
-repos="git@github.com:elementalvoid/dotfiles git@github.com:elementalvoid/dotfiles-private"
-for repo in ${repos}; do
-  if homeshick list | grep -q ${repo}; then
-    homeshick --batch pull ${repo/*\//}
-  else
-    homeshick --batch clone ${repo}
-  fi
-done
-homeshick --force link
+sh -c "$(curl -fsLS git.io/chezmoi)" -- -b ~/.local/bin/ init --apply elementalvoid/dotfiles
+sh -c "$(curl -fsLS git.io/chezmoi)" -- -b ~/.local/bin/ init --apply elementalvoid/dotfiles-private
 
 ##
 # Vim
@@ -53,19 +39,17 @@ fi
 ##
 # asdf
 ##
-asdf_plugins="argo awscli bitwarden brig conftest eksctl golang gradle helm helm-docs helmfile hub java jq kubectl kustomize linkerd nodejs nova octant pluto poetry popeye python ruby saml2aws sinker sops sopstool stern terraform terraform-docs terraform-validator tflint tfsec tmux yq"
 if [[ -d ~/.asdf ]]; then
   source ~/.asdf/asdf.sh
   asdf update
 else
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
-  asdf update
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 fi
 
 set +e
 asdf plugin add util git@github.com:elementalvoid/asdf-util.git
 
-for plugin in ${asdf_plugins}; do
+for plugin in $(awk '{print $1}' ~/.tool-versions); do
   asdf plugin add ${plugin}
 done
 
