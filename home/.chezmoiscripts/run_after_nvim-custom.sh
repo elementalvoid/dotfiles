fa57da9f437adbe8cf4f69b634a279ec22559bac
Matt Klich <matt@elementalvoid.com>
2022-12-08 16:14:06 -0700

    Bye NvChad. Hello AstroNvim!

--- home/.chezmoiscripts/run_after_nvim-custom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -d ~/.config/nvim ] && [ -d ~/.config/nvim-custom ]; then
-  if [ ! -d ~/.config/nvim/lua/custom ]; then
-    echo "Configuring nvim-custom"
-    ln -s ~/.config/nvim-custom/lua/custom ~/.config/nvim/lua/custom
-  fi
-fi
