06500b4e3bcd7c95740b2ff9fa5142b40529d569
Matt Klich <matt@elementalvoid.com>
2024-03-07 15:45:30 -0700

    Add wezterm config

---
+++ home/dot_wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'catppuccin-latte'
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.scrollback_lines = 10000
+config.audible_bell = "Disabled"
+
+-- Customize hyperlinks: 
+--   https://wezfurlong.org/wezterm/hyperlinks.html#implicit-hyperlinks
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- Disabled for now because _path_ names match the regex
+-- table.insert(config.hyperlink_rules, {
+--   regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+--   format = 'https://www.github.com/$1/$3',
+-- })
+
+return config
