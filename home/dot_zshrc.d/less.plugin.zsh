6a10bc7d111a6d16fb15bdad59b5094600bcb746
Matt Klich <matt@elementalvoid.com>
2024-12-26 21:23:44 -0700

    Guard brew, have mise install early

--- home/dot_zshrc.d/less.plugin.zsh
+++ home/dot_zshrc.d/less.plugin.zsh
@@ -5,6 +5,10 @@ export LESS="-R"
 
 # make less more friendly for non-text input files, see lesspipe(1)
 #[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-export LESSOPEN="|$(brew --prefix)/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
+if [[ $OSTYPE =~ darwin.* ]]; then
+  export LESSOPEN="|$(brew --prefix)/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
+else
+  [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+fi
 
 # vim: set ft=sh ts=2 sw=2 tw=0 :
